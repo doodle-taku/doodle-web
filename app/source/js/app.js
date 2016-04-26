9f2442b1ed58da5d0638883bf8c53fe9a4054974
松本 和馬 <hello@mtmtkzm.com>
2016-04-27 04:46:04 +0900

    topSVGアニメーションと本文追加

--- app/source/js/app.js
+++ app/source/js/app.js
@@ -1,30 +1,28 @@
 /* ロゴアニメーション
 ************************* */
-var speed = 1000;
-var svg = Snap(600,300).remove().attr({viewBox: [0, 0, 600, 300]});
+var x = 100;
+var speed = 9;
 
-Object.keys(paths).forEach(function (key) {
+var de = document.getElementById('deviation-element');
 
-	var path = svg.path(paths[key].from);
-
-	(function () {
-		path.attr({
-			path: paths[key].from,
-			fill: '#5782B2',
-		}).animate({
-			path: paths[key].to
-		}, speed, function() {
-			$('#logo').fadeOut(1000, fadeContents);
-		});
-	}());
-});
-
-svg.prependTo(logo);
+function removeBlur () {
+	x--;
+	if(x == 0){
+		$('#js-svg').fadeOut();
+		setTimeout(fadeContents, 1000);
+		return;
+	}
+	de.setAttribute('stdDeviation', x);
+}
+setInterval(removeBlur, speed);
 
 /* コンテンツ表示アニメーション
 ************************* */
 function fadeContents() {
 	$('#js-contents').fadeIn(1000, function() {
-		$('.js-typeAnime').t();
+		$('.js-type').t({
+			speed: 100,
+			caret: false
+		});
 	});
 }
