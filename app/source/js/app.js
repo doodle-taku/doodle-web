c88e4e87f670be83135880bbee31a5a1398e1499
松本 和馬 <hello@mtmtkzm.com>
2016-04-27 02:44:08 +0900

    現状

--- app/source/js/app.js
+++ app/source/js/app.js
@@ -1,24 +1,22 @@
 /* ロゴアニメーション
 ************************* */
-var speed = 400;
+var speed = 1000;
 var svg = Snap(600,300).remove().attr({viewBox: [0, 0, 600, 300]});
 
 Object.keys(paths).forEach(function (key) {
-	var path = svg.path(paths[key].from).attr({fill: paths[key].color});
-	function animatePath() {
-		path.stop().attr({
+
+	var path = svg.path(paths[key].from);
+
+	(function () {
+		path.attr({
 			path: paths[key].from,
-			fill: '#5782B2'
+			fill: '#5782B2',
 		}).animate({
-			path: paths[key].to,
-			fill: paths[key].color,
-			opacity: paths[key].opacity
-		}, speed, function(){
-			$('#logo').fadeOut(1000,fadeContents);
-			$('#logo-bg').fadeOut(1000,fadeContents);
+			path: paths[key].to
+		}, speed, function() {
+			$('#logo').fadeOut(1000, fadeContents);
 		});
-	}
-	setTimeout(animatePath, 1500);
+	}());
 });
 
 svg.prependTo(logo);
