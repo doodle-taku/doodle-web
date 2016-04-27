873121d682cfd031fe56c3c20866c27124c4957b
松本 和馬 <hello@mtmtkzm.com>
2016-04-27 14:06:33 +0900

    最低限の文言をかいた

--- app/source/js/app.js
+++ app/source/js/app.js
@@ -1,28 +1,15 @@
-/* ロゴアニメーション
-************************* */
-var x = 100;
-var speed = 9;
-
-var de = document.getElementById('deviation-element');
-
-function removeBlur () {
-	x--;
-	if(x == 0){
-		$('#js-svg').fadeOut();
-		setTimeout(fadeContents, 1000);
-		return;
-	}
-	de.setAttribute('stdDeviation', x);
-}
-setInterval(removeBlur, speed);
-
 /* コンテンツ表示アニメーション
 ************************* */
 function fadeContents() {
-	$('#js-contents').fadeIn(1000, function() {
-		$('.js-type').t({
-			speed: 100,
-			caret: false
-		});
+	var delay = 6200;
+	$('.js-type').t({
+		speed: 100,
+		caret: false
 	});
+	setTimeout(function(){
+		$('.js-fadein').animate({
+			opacity: 1,
+		}, 500);
+	}, 6200);
 }
+fadeContents();
