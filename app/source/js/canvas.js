8eda47f506fa03b74984cb4b8bbf9918e46b2421
松本 和馬 <hello@mtmtkzm.com>
2016-04-28 12:42:27 +0900

    いらないファイルを削除

--- app/source/js/canvas.js
+++
@@ -1,9 +0,0 @@
-/* Resize,Load時にcanvasを全画面表示
------------------------------- */
-// function fitWindow() {
-// 	var ww = $(window).width();
-// 	var wh = $(window).height();
-// 	$('#stage').get(0).width = ww;
-// 	$('#stage').get(0).height = wh;
-// }
-// $(window).on('load resize', fitWindow);
