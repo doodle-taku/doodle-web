bf02fec7484488d70b1ccf079cfa95a2b4d1b904
松本 和馬 <hello@mtmtkzm.com>
2016-06-05 04:17:21 +0900

    トップのメンバーリストがランダムで表示されるように

--- app/source/js/app.js
+++ app/source/js/app.js
@@ -1,10 +1,37 @@
 /* コンテンツ表示アニメーション
-************************* */
+ ************************* */
 function fadeContents() {
-	var delay = 6200;
-	$('.js-fadein').animate({
-		opacity: 1,
-	}, 500);
+    var delay = 6200;
+    $('.js-fadein').animate({
+        opacity: 1,
+    }, 500);
 }
 
-setTimeout(fadeContents, 1000);
+// まずは関連する配列を用意
+var members = []; // 初期表示順
+var randomMembers = []; // ランダム表示順
+
+// 各要素を取得
+$('.js-randomList').each(function() {
+    members.push($(this).prop('outerHTML'));
+});
+
+// 配列を並び替える
+var randomMembers = shuffle(members);
+function shuffle(array) {
+    for(var i = array.length - 1; i > 0; i--){
+        var r = Math.floor(Math.random() * (i+1));
+        var tmp = array[i];
+        array[i] = array[r];
+        array[r] = tmp;
+    }
+    return array;
+}
+
+// 元の表示をクリア（削除）
+$('.js-randomList').remove();
+
+// シャッフルされた配列をul.js-randomに追加
+for(var i=0; i<randomMembers.length; i++) {
+    $('.js-random').append(randomMembers[i]);
+}
