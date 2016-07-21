44f78ccbf4994b5b0d2f88a9266caf6f9de7b79f
松本 和馬 <hello@mtmtkzm.com>
2016-07-21 14:52:22 +0900

    commonsとして共通jsと別のjsを分離

--- app/source/js/index.js
+++ app/source/js/index.js
@@ -1,23 +1,2 @@
 // index.js
 
-var selection; // 選択中の検索ワード
-var parameter = 'none'; // ページ遷移時のパラメータ
-
-$('.js-form').on('click', function() {
-    $('.js-words').show();
-});
-
-// WordListのクリックでそのwordを選択する
-
-$('.js-word-list li').on('click', function() {
-    selection = $(this).context.innerHTML;
-    $('.js-form input').val(selection);
-    $('.js-words').hide();
-});
-
-
-$('.js-searchBtn').on('click', function() {
-    parameter = selection;
-    console.log(parameter);
-    window.location.href = './s.html?p='+parameter;
-});
