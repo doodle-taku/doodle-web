9f3ff81ec40548b150a724871008ba9da6aca8ba
松本 和馬 <hello@mtmtkzm.com>
2016-07-21 17:04:42 +0900

    検索結果画面のフォームないに値を代入

--- app/source/js/s.js
+++ app/source/js/s.js
@@ -1,6 +1,7 @@
-// index.js
+// s.js
 
 var param = window.location.search;
 var decoded = decodeURIComponent(param); // URLデコード
 decoded = decoded.substr(3); // ?p= を切り取る
-console.log(decoded);
+
+$('.js-form input').val(decoded);
