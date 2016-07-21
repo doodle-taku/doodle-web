21f2175ae0e676c6c124c5edf3b6b9a319875bbd
松本 和馬 <hello@mtmtkzm.com>
2016-07-21 18:30:48 +0900

    候補リスト以外をクリックで候補リストを非表示に

--- app/source/js/common.js
+++ app/source/js/common.js
@@ -3,21 +3,31 @@
 var selection = ' '; // 選択中の検索ワード
 var parameter = 'none'; // ページ遷移時のパラメータ
 
-$('.js-form').on('click', function() {
-	$(this).css('border-color', '#3b99fc');
+$('.js-input').on('click', function() {
+	$('.js-form').css('border-color', '#3b99fc');
 	// 単語候補の位置合わせ
 	$('.js-words').css({
 		top: $('.js-form').offset().top + $('.js-form').outerHeight(),
-		left: $('.js-form').offset().left
+		left: $('.js-form').offset().left,
+		display: 'block'
 	});
-	$('.js-words').show();
 });
 
 // WordListからwordを選択
-$('.js-word-list li').on('click', function() {
-	selection = $(this).context.innerHTML;
-	$('.js-form input').val(selection);
-	$('.js-words').hide();
+$(window).on('click', function(e) {
+	var target = $(e.target);
+	console.log();
+	if (target.hasClass('js-input')) { 
+		$('.js-words').show();
+	} else if (target.offsetParent().hasClass('js-word-list')) {
+		selection = target.context.innerHTML;
+		$('.js-input').val(selection);
+		$('.js-words').hide();
+	}  else { // 候補ワードリスト以外を触った時
+		$('.js-words').hide();
+		$('.js-form').css('border-color', '#d9d9d9');
+	}
+
 });
 
 // パラメータをもたせてページ遷移
--- app/source/js/s.js
+++ app/source/js/s.js
@@ -4,4 +4,4 @@ var param = window.location.search;
 var decoded = decodeURIComponent(param); // URLデコード
 decoded = decoded.substr(3); // ?p= を切り取る
 
-$('.js-form input').val(decoded);
+$('.js-input').val(decoded);
