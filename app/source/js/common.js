26e31b10e89ea27c04b9348d7ad99c5d999a1512
松本 和馬 <hello@mtmtkzm.com>
2016-07-21 17:28:51 +0900

    単語候補リストの位置合わせをjsで

--- app/source/js/common.js
+++ app/source/js/common.js
@@ -1,23 +1,27 @@
 // common.js
 
-var selection; // 選択中の検索ワード
+var selection = ' '; // 選択中の検索ワード
 var parameter = 'none'; // ページ遷移時のパラメータ
 
 $('.js-form').on('click', function() {
-    $('.js-words').show();
+	$(this).css('border-color', '#3b99fc');
+	// 単語候補の位置合わせ
+	$('.js-words').css({
+		top: $('.js-form').offset().top + $('.js-form').outerHeight(),
+		left: $('.js-form').offset().left
+	});
+	$('.js-words').show();
 });
 
-// WordListのクリックでそのwordを選択する
-
+// WordListからwordを選択
 $('.js-word-list li').on('click', function() {
-    selection = $(this).context.innerHTML;
-    $('.js-form input').val(selection);
-    $('.js-words').hide();
+	selection = $(this).context.innerHTML;
+	$('.js-form input').val(selection);
+	$('.js-words').hide();
 });
 
-
+// パラメータをもたせてページ遷移
 $('.js-searchBtn').on('click', function() {
-    parameter = selection;
-    console.log(parameter);
-    window.location.href = './s.html?p='+parameter;
+	parameter = selection;
+	window.location.href = './s.html?p=' + parameter;
 });
