60449a3f747c5841cfe847cbc6d0dbd0b6190273
松本 和馬 <hello@mtmtkzm.com>
2016-07-24 22:43:30 +0900

    SessionStorageでやりとり

--- app/source/js/common.js
+++ app/source/js/common.js
@@ -1,7 +1,8 @@
 // common.js
 
 var selection = ' '; // 選択中の検索ワード
-var parameter = 'none'; // ページ遷移時のパラメータ
+var parameter = ''; // ページ遷移時のパラメータ
+var d_word = '';
 
 $('.js-input').on('click', function() {
 	$('.js-form').css('border-color', '#3b99fc');
@@ -16,10 +17,10 @@ $('.js-input').on('click', function() {
 // WordListからwordを選択
 $(window).on('click', function(e) {
 	var target = $(e.target);
-	console.log();
-	if (target.hasClass('js-input')) { 
+	if (target.hasClass('js-input')) {
 		$('.js-words').show();
 	} else if (target.offsetParent().hasClass('js-word-list')) {
+		d_word = target.context.dataset.word;
 		selection = target.context.innerHTML;
 		$('.js-input').val(selection);
 		$('.js-words').hide();
@@ -33,4 +34,7 @@ $(window).on('click', function(e) {
 $('.js-searchBtn').on('click', function() {
 	parameter = selection;
 	window.location.href = './s.html?p=' + parameter;
+
+	// SessionStrageにdata-wordを一時保存
+	window.sessionStorage.setItem('word' , d_word);
 });
--- app/source/js/s.js
+++ app/source/js/s.js
@@ -1,7 +1,21 @@
 // s.js
 
+// parameter から 文字列を抽出して検索フォームのValueにいれておく。
 var param = window.location.search;
 var decoded = decodeURIComponent(param); // URLデコード
 decoded = decoded.substr(3); // ?p= を切り取る
+$('.js-input').val(decoded); // 検索フォームに挿入
 
-$('.js-input').val(decoded);
+if(window.sessionStorage) { // ウェブストレージに対応している
+	var s_storage = window.sessionStorage;
+	var e_d_word = sessionStorage.getItem('word');
+
+	$('.js-article-list').find('article').each(function() {
+		if ($(this).data('word') == e_d_word) {
+			// 記事とフォームvalueのdata属性が一致したら、そのDOMを移動
+			$(this).prependTo('.js-article-list');
+		}
+	});
+} else { // WebStorageに対応していない：適当に並び替える
+	console.log('お使いのブラウザは、WebStorageに対応していません。');
+}
