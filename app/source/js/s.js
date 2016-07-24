37273979daae10554b3b0862524690f4098fee52
松本 和馬 <hello@mtmtkzm.com>
2016-07-24 22:52:32 +0900

    並び替え

--- app/source/js/s.js
+++ app/source/js/s.js
@@ -6,6 +6,14 @@ var decoded = decodeURIComponent(param); // URLデコード
 decoded = decoded.substr(3); // ?p= を切り取る
 $('.js-input').val(decoded); // 検索フォームに挿入
 
+
+// 検索結果それぞれを適当に並び替える
+var $rList = $('.js-article-list').find('article');
+$rList = $rList.sort(function(){ return Math.random() - Math.random(); });
+$('.js-article-list').find('article').remove();
+$('.js-article-list').append($rList);
+
+// 検索該当作品を一番上に
 if(window.sessionStorage) { // ウェブストレージに対応している
 	var s_storage = window.sessionStorage;
 	var e_d_word = sessionStorage.getItem('word');
@@ -16,6 +24,4 @@ if(window.sessionStorage) { // ウェブストレージに対応している
 			$(this).prependTo('.js-article-list');
 		}
 	});
-} else { // WebStorageに対応していない：適当に並び替える
-	console.log('お使いのブラウザは、WebStorageに対応していません。');
 }
