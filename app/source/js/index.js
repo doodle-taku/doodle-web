e0d9304d5f0e4b5492b2538cd35200585bb06716
松本 和馬 <hello@mtmtkzm.com>
2016-08-18 04:20:12 +0900

    トップページのdoodleをランダムに表示

--- app/source/js/index.js
+++ app/source/js/index.js
@@ -1,2 +1,17 @@
 // index.js
 
+// doodleのランダム表示
+var randomDoodleSrc = [
+	'takeda.gif',
+	'semmu.gif',
+	'ikebe.png',
+	'cheese.png',
+	'mei.png',
+	'mun.png',
+	'eguchi.png',
+	'paul.png'
+]
+var randomNum = Math.floor(Math.random()*8);
+
+var randomDoodle = './images/doodle/' + randomDoodleSrc[randomNum];
+$('.js-main-logo').attr('src', randomDoodle);
